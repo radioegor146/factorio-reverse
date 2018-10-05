2b22a0ab62d6bc71b81bb3be76fda3ef0d62a974
Roman <roma-svistunov@tut.by>
2018-10-05 23:30:46 +0300

    Packets fix commit

---
+++ Network/FactorioNetParser/FactorioNetParser/IWritable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorioNetParser
+{
+    public interface IWritable<T>
+    {
+        void Write(System.IO.BinaryWriter writer);
+    }
+}
